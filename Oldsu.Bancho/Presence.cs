46f20f4d876d84e7690066f871c589739d0badda
jeesusmies <burnerthingxd@gmail.com>
2021-06-22 14:44:10 +0300

    Finish Presence class.

--- Oldsu.Bancho/Presence.cs
+++ Oldsu.Bancho/Presence.cs
@@ -1,7 +1,15 @@
-﻿namespace Oldsu.Bancho
+﻿using Oldsu.Enums;
+
+namespace Oldsu.Bancho
 {
     public class Presence
     {
-        public Privilege
+        public Privileges Privilege;
+        
+        public byte UtcOffset;
+        public byte Country; // find out is this byte or string
+        
+        public float Longitude;
+        public float Latitude;
     }
 }
