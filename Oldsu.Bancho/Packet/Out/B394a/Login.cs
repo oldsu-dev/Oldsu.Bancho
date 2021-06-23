1cf81a80bc1991062b5ab6e6ada1527cb1aed3de
jeesusmies <burnerthingxd@gmail.com>
2021-06-24 00:46:28 +0300

    Make login packet for b394a.

---
+++ Oldsu.Bancho/Packet/Out/B394a/Login.cs
@@ -0,0 +1,11 @@
+﻿namespace Oldsu.Bancho.Packet.Out.B394a
+{
+    public struct Login : IB394APacketOut
+    {
+        [BanchoSerializable] 
+        public int LoginStatus;
+
+        [BanchoSerializable] 
+        public byte Privilege;
+    }
+}
