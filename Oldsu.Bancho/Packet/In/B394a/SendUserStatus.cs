e8e7cfa651c48b40c928b8ef0212ac98cb505acc
jeesusmies <burnerthingxd@gmail.com>
2021-06-23 16:28:50 +0300

    Add SendUserStatus packet handler.

---
+++ Oldsu.Bancho/Packet/In/B394a/SendUserStatus.cs
@@ -0,0 +1,36 @@
+﻿using Oldsu.Bancho.Objects;
+using Oldsu.Bancho.Packet.Shared;
+
+namespace Oldsu.Bancho.Packet.In.B394a
+{
+    public struct SendUserStatus : Into<ISharedPacket>
+    {
+        public byte Status;
+        public bool IsBeatmapUpdate;
+        public BeatmapUpdate BeatmapUpdate;
+
+        ISharedPacket Into<ISharedPacket>.Into()
+        {
+            var userActivity = new UserActivity();
+
+            userActivity.Status = Status;
+            userActivity.Gamemode = 0;
+            userActivity.MapID = 0;
+            
+            if (IsBeatmapUpdate)
+            {
+                userActivity.Map = BeatmapUpdate.Map ;
+                userActivity.MapSHA256 = BeatmapUpdate.MapSha256;
+                userActivity.Mods = BeatmapUpdate.Mods;
+            }
+            else
+            {
+                userActivity.Map = "";
+                userActivity.MapSHA256 = "";
+                userActivity.Mods = 0;
+            }
+
+            return userActivity;
+        }
+    }
+}
