9d63ee4c38a75a37d9931a9e3ea8d0cf87f4d9f0
jeesusmies <burnerthingxd@gmail.com>
2021-06-24 00:48:58 +0300

    Make shared login packet.

---
+++ Oldsu.Bancho/Packet/Shared/Login.cs
@@ -0,0 +1,24 @@
+﻿namespace Oldsu.Bancho.Packet.Shared
+{
+    public class Login : ISharedPacket, Into<IB394APacketOut>
+    {
+        public int LoginStatus;
+        public byte Privilege;
+        
+        public Login(params object[] fields)
+        {
+            LoginStatus = (int)fields[0];
+            Privilege = (byte)fields[1];
+        }
+
+        public IB394APacketOut Into()
+        {
+            var packet = new Out.B394a.Login();
+            
+            packet.LoginStatus = LoginStatus;
+            packet.Privilege = Privilege;
+
+            return packet;
+        }
+    }
+}
