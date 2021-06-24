6119c5f30c04800e2ef1519ecb19b35d20c111f9
jeesusmies <burnerthingxd@gmail.com>
2021-06-24 21:51:48 +0300

    Rename to bStatusUpdate, add 2 more data fields and add attributes to them.

---
+++ Oldsu.Bancho/Objects/bStatusUpdate.cs
@@ -0,0 +1,11 @@
+﻿namespace Oldsu.Bancho.Objects
+{
+    public struct bStatusUpdate
+    {
+        [BanchoSerializable] public byte bStatus;
+        [BanchoSerializable] public bool BeatmapUpdate;
+        [BanchoSerializable] public string Map;
+        [BanchoSerializable] public string MapSha256;
+        [BanchoSerializable] public ushort Mods;
+    }
+}
