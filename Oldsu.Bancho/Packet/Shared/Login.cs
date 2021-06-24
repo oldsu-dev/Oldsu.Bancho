494360e76b60aa8a31b52f7588ab551f75a20c2a
jeesusmies <burnerthingxd@gmail.com>
2021-06-24 13:14:15 +0300

    Remove constructor and use fields instead on Login.

--- Oldsu.Bancho/Packet/Shared/Login.cs
+++ Oldsu.Bancho/Packet/Shared/Login.cs
@@ -1,15 +1,9 @@
 ﻿namespace Oldsu.Bancho.Packet.Shared
 {
-    public class Login : ISharedPacket, Into<IB394APacketOut>
+    public struct Login : ISharedPacket, Into<IB394APacketOut>
     {
-        public int LoginStatus;
-        public byte Privilege;
-        
-        public Login(params object[] fields)
-        {
-            LoginStatus = (int)fields[0];
-            Privilege = (byte)fields[1];
-        }
+        public int LoginStatus { get; set; }
+        public byte Privilege { get; set; }
 
         public IB394APacketOut Into()
         {
