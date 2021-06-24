9bd28719a8d349c7533628663d4b25d87ec094d5
jeesusmies <burnerthingxd@gmail.com>
2021-06-24 21:23:39 +0300

    Add Status class.

---
+++ Oldsu.Bancho/Status.cs
@@ -0,0 +1,22 @@
+﻿namespace Oldsu.Bancho
+{
+    public class Status
+    {
+        public byte bStatus { get; set; }
+        public byte Gamemode { get; set; }
+        public ushort Mods { get; set; }
+        public string Map { get; set; }
+        public string MapMD5 { get; set; }
+        public int MapID { get; set; }
+        
+        public Status()
+        {
+            bStatus = 0;
+            Gamemode = 0;
+            Mods = 0;
+            Map = "";
+            MapMD5 = "";
+            MapID = 0;
+        }
+    }
+}
