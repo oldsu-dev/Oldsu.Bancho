1c30f846efb4e58ad8de938adaf88c5f53219937
jeesusmies <burnerthingxd@gmail.com>
2021-06-24 21:54:49 +0300

    Create HandleOsuUpdate packets.

---
+++ Oldsu.Bancho/Packet/Out/B394a/HandleOsuUpdateSelf.cs
@@ -0,0 +1,18 @@
+﻿using Oldsu.Bancho.Objects;
+using Oldsu.Enums;
+
+namespace Oldsu.Bancho.Packet.Out.B394a
+{
+    [BanchoPacket(12, Version.B394A, BanchoPacketType.Out)]
+    public class HandleOsuUpdateSelf : IB394APacketOut
+    {
+        [BanchoSerializable] public int UserID;
+        [BanchoSerializable] public byte Completeness = 0;
+        [BanchoSerializable] public bStatusUpdate BStatusUpdate;
+        [BanchoSerializable] public long RankedScore;
+        [BanchoSerializable] public float Accuracy;
+        [BanchoSerializable] public int Playcount;
+        [BanchoSerializable] public long TotalScore;
+        [BanchoSerializable] public ushort Rank;
+    }
+}
