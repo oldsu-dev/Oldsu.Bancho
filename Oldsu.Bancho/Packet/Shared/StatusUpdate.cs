d65bcf7e24c1c687c2493d309a1d5926484f60c1
jeesusmies <burnerthingxd@gmail.com>
2021-06-25 19:07:55 +0300

    Change dividing number to 100 and add a null check for Stats.

--- Oldsu.Bancho/Packet/Shared/StatusUpdate.cs
+++ Oldsu.Bancho/Packet/Shared/StatusUpdate.cs
@@ -13,23 +13,47 @@ namespace Oldsu.Bancho.Packet.Shared
 
         public IB394APacketOut Into()
         {
-            var packet = new HandleOsuUpdateSelf
+            HandleOsuUpdateSelf packet;
+            if (Stats != null) 
             {
-                UserID = (int)User.UserID,
-                RankedScore = (long)Stats.RankedScore,
-                TotalScore = (long)Stats.TotalScore,
-                Playcount = (int)Stats.Playcount,
-                Accuracy = Stats.Accuracy / 1000f,
-                Rank = 0,
-                BStatusUpdate = new bStatusUpdate
+                packet = new HandleOsuUpdateSelf
                 {
-                    bStatus = Status.bStatus,
-                    BeatmapUpdate = true,
-                    Map = Status.Map,
-                    MapSha256 = Status.MapSha256,
-                    Mods = Status.Mods,
-                }
-            };
+                    UserID = (int)User.UserID,
+                    RankedScore = (long)Stats.RankedScore,
+                    TotalScore = (long)Stats.TotalScore,
+                    Playcount = (int)Stats.Playcount,
+                    Accuracy = Stats.Accuracy / 100f,
+                    Rank = 0,
+                    BStatusUpdate = new bStatusUpdate
+                    {
+                        bStatus = Status.bStatus,
+                        BeatmapUpdate = true,
+                        Map = Status.Map,
+                        MapSha256 = Status.MapSha256,
+                        Mods = Status.Mods,
+                    }
+                };
+            }
+            else 
+            {
+                packet = new HandleOsuUpdateSelf
+                {
+                    UserID = (int)User.UserID,
+                    RankedScore = 0,
+                    TotalScore = 0,
+                    Playcount = 0,
+                    Accuracy = Stats.Accuracy / 100f,
+                    Rank = 0,
+                    BStatusUpdate = new bStatusUpdate
+                    {
+                        bStatus = Status.bStatus,
+                        BeatmapUpdate = true,
+                        Map = Status.Map,
+                        MapSha256 = Status.MapSha256,
+                        Mods = Status.Mods,
+                    }
+                };
+            }
 
             return packet;
         }
