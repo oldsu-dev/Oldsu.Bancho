c488b128c7f0560d4359ecb3885a7230b32bf539
jeesusmies <burnerthingxd@gmail.com>
2021-06-27 13:44:49 +0300

    Correct namespace.

--- Oldsu.Bancho/Packet/In/B394a/UserStatsRequest.cs
+++ Oldsu.Bancho/Packet/In/B394a/UserStatsRequest.cs
@@ -3,8 +3,8 @@
 namespace Oldsu.Bancho.Packet.In.B394a
 {
     [BanchoPacket(3, Version.B394A, BanchoPacketType.In)]
-    public struct UserStatsRequest : Into<Shared.UserStatsRequest>
+    public struct UserStatsRequest : Into<Shared.In.UserStatsRequest>
     {
-        public Shared.UserStatsRequest Into() => new();
+        public Shared.In.UserStatsRequest Into() => new();
     }
 }
