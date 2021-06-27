036ecac2385960889f73d5f976842e7e676591c4
jeesusmies <burnerthingxd@gmail.com>
2021-06-27 13:41:48 +0300

    Change UserStatsRequest to a struct to fit with consistency.

--- Oldsu.Bancho/Packet/In/B394a/UserStatsRequest.cs
+++ Oldsu.Bancho/Packet/In/B394a/UserStatsRequest.cs
@@ -3,7 +3,7 @@
 namespace Oldsu.Bancho.Packet.In.B394a
 {
     [BanchoPacket(3, Version.B394A, BanchoPacketType.In)]
-    public class UserStatsRequest : Into<Shared.UserStatsRequest>
+    public struct UserStatsRequest : Into<Shared.UserStatsRequest>
     {
         public Shared.UserStatsRequest Into() => new();
     }
