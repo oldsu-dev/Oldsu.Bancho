1d13c2948e3d1c1eaca7655bb09c56c5b2fc29b5
jeesusmies <burnerthingxd@gmail.com>
2021-07-13 20:25:10 +0300

    Add quit and send user status packet.

---
+++ Oldsu.Bancho/Packet/In/B904/Quit.cs
@@ -0,0 +1,10 @@
+﻿using Oldsu.Enums;
+
+namespace Oldsu.Bancho.Packet.In.B904
+{
+    [BanchoPacket(13, Version.B904, BanchoPacketType.In)]
+    public struct Quit : Into<Shared.In.Quit>
+    {
+        Shared.In.Quit Into<Shared.In.Quit>.Into() => new();
+    }
+}
