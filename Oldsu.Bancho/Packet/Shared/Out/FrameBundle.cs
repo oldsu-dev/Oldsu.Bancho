77e31176e50303eb70d303c136f6c28bf949e132
jeesusmies <burnerthingxd@gmail.com>
2021-07-15 22:33:08 +0300

    Finish boilerplate spectating.

---
+++ Oldsu.Bancho/Packet/Shared/Out/FrameBundle.cs
@@ -0,0 +1,17 @@
+﻿namespace Oldsu.Bancho.Packet.Shared.Out
+{
+    public class FrameBundle : ISharedPacketOut, Into<IGenericPacketOut>
+    {
+        public byte[] Frames { get; init; }
+
+        public IGenericPacketOut Into()
+        {
+            var packet = new Packet.Out.Generic.SendFrameBundle
+            {
+                Frames = Frames
+            };
+
+            return packet;
+        }
+    }
+}
