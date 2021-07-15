71c53a86d751da7a88f28777ff735cd790f381bc
jeesusmies <burnerthingxd@gmail.com>
2021-07-15 23:36:33 +0300

    Extremely unstable spectating code.

--- Oldsu.Bancho/Packet/In/Generic/FrameBundle.cs
+++ Oldsu.Bancho/Packet/In/Generic/FrameBundle.cs
@@ -3,15 +3,13 @@
 namespace Oldsu.Bancho.Packet.In.Generic
 {
     [BanchoPacket(19, Version.NotApplicable, BanchoPacketType.In)]
-    public struct FrameBundle : Into<Shared.In.FrameBundle>, IGenericPacketIn
+    public class FrameBundle : BanchoBuffer, Into<Shared.In.FrameBundle>, IGenericPacketIn
     {
-        [BanchoSerializable] public byte[] Frames;
-        
         public Shared.In.FrameBundle Into()
         {
             return new Shared.In.FrameBundle
             {
-                Frames = Frames
+                Frames = Data
             };
         }
     }
